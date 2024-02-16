6cd1059e287b8ede803b694e6ea4dea2f31d2663
Avi Vashishta <avivashishta@gmail.com>
2024-02-16 16:27:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let btns = document.querySelectorAll(".btn");
+
+for(let i=0;i<btns.length;i++)
+{
+	btns[i].addEventListener("click",()=>{
+		stopSounds();
+		let audio = document.getElementById(btns[i].innerText);
+		audio.play();
+	})
+	
+}
+
+let stop = document.querySelector(".stop");
+stop.addEventListener("click",()=>{
+	stopSounds();
+})
+
+
+function stopSounds(){
+	for(let i=0;i<btns.length;i++)
+{
+	let audio = document.getElementById(btns[i].innerText);
+		audio.pause();
+
+}
+}
